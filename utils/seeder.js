0ed77a4d13e3e3d22eebdd05104f44cf72707492
margaiwangara <margaiwangara@gmail.com>
2019-11-15 18:05:37 +0300

    created a products seeder with a data folder to store all products data, working successfully

---
+++ utils/seeder.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// dotenv file loc
+dotenv.config({ path: `${__dirname}/../config/config.env` });
+
+// set debug to true to display db responses
+mongoose.set("debug", true);
+// enable promises for mongoose
+mongoose.Promise = Promise;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    keepAlive: true
+  })
+  .then(conn => console.log(`MongoDB Connected: ${conn.connection.host}`))
+  .catch(error => console.log(error));
+
+// Collections
+const Products = require("../models/product");
+
+const seedProductsCollection = async () => {
+  try {
+    const data = fs.readFileSync(`${__dirname}/../_data/products.json`, {
+      encoding: "utf-8"
+    });
+    await Products.create(JSON.parse(data));
+
+    console.log("Data seeding complete");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteProductsFromCollection = async () => {
+  try {
+    await Products.deleteMany();
+
+    console.log("Deleted all product records");
+  } catch (error) {
+    console.log(error);
+  }
+};
+if (process.argv[2] === "-c") {
+  seedProductsCollection();
+} else if (process.argv[2] === "-d") {
+  deleteProductsFromCollection();
+}
